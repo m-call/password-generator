b5d247ec37aeab34503f893edfc49f87acd7b69d
Michael Callahan <mcallahan0@yahoo.com>
2021-06-18 16:02:14 -0400

    Removed unnecessary arrays and fixed the for loop to make the generator work properly. Then, added the code with the for loop into each if/else statement to work for every possible combination of lower, upper, number, and special depending on what the user chooses.

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,18 +11,11 @@ function writePassword() {
 
 function generatePassword() {
 
-  // var test = "abcde".split("");
-
   var passwordLength = prompt("How many characters would you like your password to be?");
-  var lowercases = "abcdefghijklmnopqrstuvwxyz";
-  var uppercases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numbers = "0123456789";
-  var specials = "`~!@#$%^&*()-_=+[]{}|;':,./<>?";
-  // var lowercaseChar = lowercases[Math.floor(Math.random()*lowercases.length)];
-  // var uppercaseChar = uppercases[Math.floor(Math.random()*uppercases.length)];
-  // var numberChar = numbers[Math.floor(Math.random()*numbers.length)];
-  // var specialChar = specials[Math.floor(Math.random()*specials.length)];
-  var allChars = [lowercaseChar = lowercases[Math.floor(Math.random()*lowercases.length)], uppercaseChar = uppercases[Math.floor(Math.random()*uppercases.length)], numberChar = numbers[Math.floor(Math.random()*numbers.length)], specialChar = specials[Math.floor(Math.random()*specials.length)]];
+  var lowercases = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var uppercases = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var specials = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", ";", "'", ":", ",", ".", "/", "<", ">", "?"];
 
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Your password must be between 8 and 128 characters!");
@@ -34,42 +27,154 @@ function generatePassword() {
   }
 
   if (hasLowercase && hasUppercase && hasNumbers && hasSpecials) {
-    var pass = "";
+    var pass = [];
 
     for (var i = 0; i < passwordLength; i++) {
-      var char = allChars[Math.floor(Math.random()*allChars.length)];
-      console.log(char);
-      pass += char
+      var allChars = [lowercases[Math.floor(Math.random()*lowercases.length)], uppercases[Math.floor(Math.random()*uppercases.length)], numbers[Math.floor(Math.random()*numbers.length)], specials[Math.floor(Math.random()*specials.length)]];
+      pass += allChars[Math.floor(Math.random()*allChars.length)];
     }
 
-    return pass
+    return pass;
+
   } else if (hasLowercase == true && hasUppercase == true && hasNumbers == true && hasSpecials == false) {
+    var pass = [];
+
+    for (var i = 0; i < passwordLength; i++) {
+      var allChars = [lowercases[Math.floor(Math.random()*lowercases.length)], uppercases[Math.floor(Math.random()*uppercases.length)], numbers[Math.floor(Math.random()*numbers.length)]];
+      pass += allChars[Math.floor(Math.random()*allChars.length)];
+    }
+
+    return pass;
 
   } else if (hasLowercase == true && hasUppercase == true && hasNumbers == false && hasSpecials == false) {
+    var pass = [];
+
+    for (var i = 0; i < passwordLength; i++) {
+      var allChars = [lowercases[Math.floor(Math.random()*lowercases.length)], uppercases[Math.floor(Math.random()*uppercases.length)]];
+      pass += allChars[Math.floor(Math.random()*allChars.length)];
+    }
+
+    return pass;
 
   } else if (hasLowercase == true && hasUppercase == false && hasNumbers == false && hasSpecials == false) {
+    var pass = [];
+
+    for (var i = 0; i < passwordLength; i++) {
+      var allChars = [lowercases[Math.floor(Math.random()*lowercases.length)]];
+      pass += allChars[Math.floor(Math.random()*allChars.length)];
+    }
+
+    return pass;
 
   } else if (hasLowercase == true && hasUppercase == false && hasNumbers == false && hasSpecials == true) {
+    var pass = [];
+
+    for (var i = 0; i < passwordLength; i++) {
+      var allChars = [lowercases[Math.floor(Math.random()*lowercases.length)], specials[Math.floor(Math.random()*specials.length)]];
+      pass += allChars[Math.floor(Math.random()*allChars.length)];
+    }
+
+    return pass;
 
   } else if (hasLowercase == true && hasUppercase == false && hasNumbers == true && hasSpecials == true) {
+    var pass = [];
+
+    for (var i = 0; i < passwordLength; i++) {
+      var allChars = [lowercases[Math.floor(Math.random()*lowercases.length)], numbers[Math.floor(Math.random()*numbers.length)], specials[Math.floor(Math.random()*specials.length)]];
+      pass += allChars[Math.floor(Math.random()*allChars.length)];
+    }
+
+    return pass;
 
   } else if (hasLowercase == true && hasUppercase == true && hasNumbers == false && hasSpecials == true) {
+    var pass = [];
+
+    for (var i = 0; i < passwordLength; i++) {
+      var allChars = [lowercases[Math.floor(Math.random()*lowercases.length)], uppercases[Math.floor(Math.random()*uppercases.length)], specials[Math.floor(Math.random()*specials.length)]];
+      pass += allChars[Math.floor(Math.random()*allChars.length)];
+    }
+
+    return pass;
 
   } else if (hasLowercase == true && hasUppercase == false && hasNumbers == true && hasSpecials == false) {
+    var pass = [];
+
+    for (var i = 0; i < passwordLength; i++) {
+      var allChars = [lowercases[Math.floor(Math.random()*lowercases.length)], numbers[Math.floor(Math.random()*numbers.length)]];
+      pass += allChars[Math.floor(Math.random()*allChars.length)];
+    }
+
+    return pass;
 
   } else if (hasLowercase == false && hasUppercase == true && hasNumbers == false && hasSpecials == false) {
+    var pass = [];
+
+    for (var i = 0; i < passwordLength; i++) {
+      var allChars = [uppercases[Math.floor(Math.random()*uppercases.length)]];
+      pass += allChars[Math.floor(Math.random()*allChars.length)];
+    }
+
+    return pass;
 
   } else if (hasLowercase == false && hasUppercase == false && hasNumbers == true && hasSpecials == false) {
+    var pass = [];
+
+    for (var i = 0; i < passwordLength; i++) {
+      var allChars = [numbers[Math.floor(Math.random()*numbers.length)]];
+      pass += allChars[Math.floor(Math.random()*allChars.length)];
+    }
+
+    return pass;
 
   } else if (hasLowercase == false && hasUppercase == false && hasNumbers == false && hasSpecials == true) {
+    var pass = [];
+
+    for (var i = 0; i < passwordLength; i++) {
+      var allChars = [specials[Math.floor(Math.random()*specials.length)]];
+      pass += allChars[Math.floor(Math.random()*allChars.length)];
+    }
+
+    return pass;
 
   } else if (hasLowercase == false && hasUppercase == true && hasNumbers == true && hasSpecials == false) {
+    var pass = [];
+
+    for (var i = 0; i < passwordLength; i++) {
+      var allChars = [uppercases[Math.floor(Math.random()*uppercases.length)], numbers[Math.floor(Math.random()*numbers.length)]];
+      pass += allChars[Math.floor(Math.random()*allChars.length)];
+    }
+
+    return pass;
 
   } else if (hasLowercase == false && hasUppercase == true && hasNumbers == false && hasSpecials == true) {
+    var pass = [];
+
+    for (var i = 0; i < passwordLength; i++) {
+      var allChars = [uppercases[Math.floor(Math.random()*uppercases.length)], specials[Math.floor(Math.random()*specials.length)]];
+      pass += allChars[Math.floor(Math.random()*allChars.length)];
+    }
+
+    return pass;
 
   } else if (hasLowercase == false && hasUppercase == false && hasNumbers == true && hasSpecials == true) {
+    var pass = [];
+
+    for (var i = 0; i < passwordLength; i++) {
+      var allChars = [numbers[Math.floor(Math.random()*numbers.length)], specials[Math.floor(Math.random()*specials.length)]];
+      pass += allChars[Math.floor(Math.random()*allChars.length)];
+    }
+
+    return pass;
 
   } else if (hasLowercase == false && hasUppercase == true && hasNumbers == true && hasSpecials == true) {
+    var pass = [];
+
+    for (var i = 0; i < passwordLength; i++) {
+      var allChars = [uppercases[Math.floor(Math.random()*uppercases.length)], numbers[Math.floor(Math.random()*numbers.length)], specials[Math.floor(Math.random()*specials.length)]];
+      pass += allChars[Math.floor(Math.random()*allChars.length)];
+    }
+
+    return pass;
     
   } else {
     alert("You must choose at least one character type!");
