49e96b1de1d5ef763951142d1050134bf48708a7
Michael Callahan <mcallahan0@yahoo.com>
2021-06-18 17:04:52 -0400

    Updated and finalized the README as well as added comments to the Javascript file.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# password-generator
+# Password Generator
+This application is a password generator that uses prompts to ask the user how many and what type of characters they want in their randomly generated password. The user is able to choose between lowecase, uppercase, numbers, and/or symbols for their generated password.
+
+Upon visiting the webpage and clicking the "Generate Password" button, the user will be first prompted to enter the number of characters they want in their password. The entry must be between 8 and 128, otherwise they will be alerted that they need to enter a value between 8 and 128. Next, they will need to confirm whether or not they want lowercase, uppercase, numbers, and/or special characters. If they choose none of the options, they will be alerted that they must choose atleast one character type. After they have entered a number between 8-128 and have chosen atleast one of the character types, they will be presented a randomly generated password with the number of characters they requested that also includes only the types of characters they requested.
+
+## Link to repository
+[GitHub](https://mcall0147.github.io/password-generator/)
+
+## Screenshot of working application
+![screenshot](./assets/images/Screenshot-2021-06-18-161839.png?raw=true "Password Generator")
--- assets/js/script.js
+++ assets/js/script.js
@@ -9,14 +9,19 @@ function writePassword() {
   passwordText.value = password;
 }
 
+// Function that generates the random password. This function is called when the "Generate Password" button is pressed.
 function generatePassword() {
 
+  // Local variables that store the length of the password that is entered by the user, as well as 4 separate arrays for lowercase, uppercase, numbers, and special characters.
   var passwordLength = prompt("How many characters would you like your password to be?");
   var lowercases = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var uppercases = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   var specials = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", ";", "'", ":", ",", ".", "/", "<", ">", "?"];
 
+  // If else statement to check if the user submitted a password length between 8 and 128.
+  // If they didn't they are prompted that the length must be between 8 and 128.
+  // If they did enter a length between 8 and 128, they are then given 4 confirm pop-ups to check whether or not they want lowercase, uppercase, numbers, and special characters.
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Your password must be between 8 and 128 characters!");
   } else {
@@ -26,14 +31,23 @@ function generatePassword() {
     var hasSpecials = confirm("Do you want special characters?");
   }
 
+  // If else if else statement that checks which of the 4 character confirms the user chose. (Lower, Upper, Numbers, Specials)
+  // If none of the confirms were chosen, then the else statement alerts the user that they must choose atleast one character type.
   if (hasLowercase && hasUppercase && hasNumbers && hasSpecials) {
+    // Creates an empty array to store and eventually return the generated password. Used in each of the else if statements.
     var pass = [];
 
+    // This loop runs as many times as the password length, the value of which was inputted by the user. Used in each of the else if statements.
     for (var i = 0; i < passwordLength; i++) {
+      // On each iteration, an array is created to hold one random value from each of the 4 character arrays. 
       var allChars = [lowercases[Math.floor(Math.random()*lowercases.length)], uppercases[Math.floor(Math.random()*uppercases.length)], numbers[Math.floor(Math.random()*numbers.length)], specials[Math.floor(Math.random()*specials.length)]];
+      
+      // On each iteration, the empty password array adds one character from the created array above at random using the Math.floor and Math.random methods.
+      // Once the loop has completed, we have our generated password.
       pass += allChars[Math.floor(Math.random()*allChars.length)];
     }
 
+    // Finally, we return the generated password so that it can be displayed on the webpage.
     return pass;
 
   } else if (hasLowercase == true && hasUppercase == true && hasNumbers == true && hasSpecials == false) {
