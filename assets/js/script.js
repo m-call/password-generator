7eb28f2f46e0ea7609fb552ef748864c541dbd56
Michael Callahan <mcallahan0@yahoo.com>
2021-06-17 23:53:50 -0400

    Changed arrays containing characters to strings. Tweaking the for loops inside the if/else statement inside the generatePassword fuction.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  console.log(password);
 }
 
 function generatePassword() {
@@ -15,61 +14,62 @@ function generatePassword() {
   // var test = "abcde".split("");
 
   var passwordLength = prompt("How many characters would you like your password to be?");
-  var lowercases = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var uppercases = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var specials = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", ";", "'", ":", ",", ".", "/", "<", ">", "?"];
-  var lowercaseChar = lowercases[Math.floor(Math.random()*lowercases.length)];
-  var uppercaseChar = uppercases[Math.floor(Math.random()*uppercases.length)];
-  var numberChar = numbers[Math.floor(Math.random()*numbers.length)];
-  var specialChar = specials[Math.floor(Math.random()*specials.length)];
-  var allChars = [lowercaseChar, uppercaseChar, numberChar, specialChar];
-  var char = allChars[Math.floor(Math.random()*allChars.length)];
+  var lowercases = "abcdefghijklmnopqrstuvwxyz";
+  var uppercases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "0123456789";
+  var specials = "`~!@#$%^&*()-_=+[]{}|;':,./<>?";
+  // var lowercaseChar = lowercases[Math.floor(Math.random()*lowercases.length)];
+  // var uppercaseChar = uppercases[Math.floor(Math.random()*uppercases.length)];
+  // var numberChar = numbers[Math.floor(Math.random()*numbers.length)];
+  // var specialChar = specials[Math.floor(Math.random()*specials.length)];
+  var allChars = [lowercaseChar = lowercases[Math.floor(Math.random()*lowercases.length)], uppercaseChar = uppercases[Math.floor(Math.random()*uppercases.length)], numberChar = numbers[Math.floor(Math.random()*numbers.length)], specialChar = specials[Math.floor(Math.random()*specials.length)]];
 
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Your password must be between 8 and 128 characters!");
   } else {
-    var lowercase = confirm("Do you want lowercase characters?");
-    var uppercase = confirm("Do you want uppercase characters?");
-    var number = confirm("Do you want numbers?");
-    var special = confirm("Do you want special characters?");
+    var hasLowercase = confirm("Do you want lowercase characters?");
+    var hasUppercase = confirm("Do you want Uppercase characters?");
+    var hasNumbers = confirm("Do you want numbers?");
+    var hasSpecials = confirm("Do you want special characters?");
   }
 
-  if (lowercase && uppercase && number && special) {
+  if (hasLowercase && hasUppercase && hasNumbers && hasSpecials) {
     var pass = "";
 
-    for (var i = 8; i <= passwordLength; i++) {
-      pass += char.repeat(passwordLength)[Math.floor(Math.random()*char.length)];
+    for (var i = 0; i < passwordLength; i++) {
+      var char = allChars[Math.floor(Math.random()*allChars.length)];
+      console.log(char);
+      pass += char
     }
 
-    return pass;
-  } else if (lowercase == true && uppercase == true && number == true && special == false) {
+    return pass
+  } else if (hasLowercase == true && hasUppercase == true && hasNumbers == true && hasSpecials == false) {
 
-  } else if (lowercase == true && uppercase == true && number == false && special == false) {
+  } else if (hasLowercase == true && hasUppercase == true && hasNumbers == false && hasSpecials == false) {
 
-  } else if (lowercase == true && uppercase == false && number == false && special == false) {
+  } else if (hasLowercase == true && hasUppercase == false && hasNumbers == false && hasSpecials == false) {
 
-  } else if (lowercase == true && uppercase == false && number == false && special == true) {
+  } else if (hasLowercase == true && hasUppercase == false && hasNumbers == false && hasSpecials == true) {
 
-  } else if (lowercase == true && uppercase == false && number == true && special == true) {
+  } else if (hasLowercase == true && hasUppercase == false && hasNumbers == true && hasSpecials == true) {
 
-  } else if (lowercase == true && uppercase == true && number == false && special == true) {
+  } else if (hasLowercase == true && hasUppercase == true && hasNumbers == false && hasSpecials == true) {
 
-  } else if (lowercase == true && uppercase == false && number == true && special == false) {
+  } else if (hasLowercase == true && hasUppercase == false && hasNumbers == true && hasSpecials == false) {
 
-  } else if (lowercase == false && uppercase == true && number == false && special == false) {
+  } else if (hasLowercase == false && hasUppercase == true && hasNumbers == false && hasSpecials == false) {
 
-  } else if (lowercase == false && uppercase == false && number == true && special == false) {
+  } else if (hasLowercase == false && hasUppercase == false && hasNumbers == true && hasSpecials == false) {
 
-  } else if (lowercase == false && uppercase == false && number == false && special == true) {
+  } else if (hasLowercase == false && hasUppercase == false && hasNumbers == false && hasSpecials == true) {
 
-  } else if (lowercase == false && uppercase == true && number == true && special == false) {
+  } else if (hasLowercase == false && hasUppercase == true && hasNumbers == true && hasSpecials == false) {
 
-  } else if (lowercase == false && uppercase == true && number == false && special == true) {
+  } else if (hasLowercase == false && hasUppercase == true && hasNumbers == false && hasSpecials == true) {
 
-  } else if (lowercase == false && uppercase == false && number == true && special == true) {
+  } else if (hasLowercase == false && hasUppercase == false && hasNumbers == true && hasSpecials == true) {
 
-  } else if (lowercase == false && uppercase == true && number == true && special == true) {
+  } else if (hasLowercase == false && hasUppercase == true && hasNumbers == true && hasSpecials == true) {
     
   } else {
     alert("You must choose at least one character type!");
